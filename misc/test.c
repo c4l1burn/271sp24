3887a62e2e180cef0e2b0ff50999b76b9ea38c8f
Cassius Ureda <user@computer.com>
2024-02-07 19:41:55 -0800

    fucked around trying to break things on purpose

--- misc/test.c
+++ misc/test.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 
+char *str1 = "salet" ;
+char *str2 = "salty" ;
+
+int str_len = 12;
+
+// "what i should do": read a character, check to see if its null,
+// print something that would tell me it is, flush std out, overwrite that
+// memory location
+
+
 void main (){
-	printf("hello, world!\n") ;
-	
+	printf("pointer of str1: %p: \n", str1) ;
+	printf("pointer of str2: %p:", str2) ;
+	printf("\n");
+
+	// iterate over a given string and print the character and its
+	// specific location in memory
+	int i;
+	//str1[1] = "q";
+	int offset = str2 - 1;
+	* offset = "q";
+	printf("\n");
+	for (int i = 0; i < str_len; i++)  {
+		printf("character %c: ,", str1[i]);
+		printf("location %x: \n", str1[i]);
+		}
+	printf(str1);
 }
+
